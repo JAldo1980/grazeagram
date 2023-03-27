2d0613f263d91693e811201f34b6ae3e71df6316
James Alderman <80925002+JAldo1980@users.noreply.github.com>
2023-03-27 18:06:13 +0100

    JS update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { posts } from "./feed-data";
+import { posts } from "./feed-data.js";
 
 const feedContainer = document.getElementById("feedContainer");
 
@@ -6,23 +6,23 @@ posts.forEach(function (post) {
   feedContainer.innerHTML += `
 <div class="feed-box">
     <div class="main-feed-header">
-    <img src="" class="main-feed-avatar" alt="contributor-avatar" />
-    <div class="main-feed-text">
-        <h3>Funny name here</h3>
-        <p>Manchester, United Kingdom</p>
-    </div>
-    </div>
-    <div class="main-feed-img-box">
-    <img src="" class="main-feed-img" alt="" />
-    </div>
-    <div class="main-feed-share-icons">
-    <img src="" alt="" />
-    <img src="" alt="" />
-    <img src="" alt="" />
-    </div>
-    <div class="main-feed-like-count">
-    <p>21, 492 likes</p>
-    <p><strong>Shawn007 </strong>Uwe must be joking?!</p>
+        <img src="${post.avatar}" class="main-feed-avatar" alt="contributor-avatar" />
+        <div class="main-feed-text">
+            <h3>${post.name}</h3>
+            <p>${post.location}</p>
+        </div>
+        </div>
+        <div class="main-feed-img-box">
+            <img src="${post.post}" class="main-feed-img" alt="" />
+        </div>
+        <div class="main-feed-share-icons">
+            <img src="images/like.png" alt="" />
+            <img src="images/comment.png" alt="" />
+            <img src="images/share.png" alt="" />
+        </div>
+        <div class="main-feed-like-count">
+            <p>${post.likes}</p>
+            <p><strong>${post.username} </strong>${post.comment}</p>
     </div>
 </div> 
     `;
