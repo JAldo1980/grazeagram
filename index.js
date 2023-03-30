4e69797433e61badd8599bce3328e8bcbfe761a2
James Alderman <80925002+JAldo1980@users.noreply.github.com>
2023-03-30 13:37:37 +0100

    final JS updates

--- index.js
+++ index.js
@@ -1,33 +1,104 @@
 import { posts } from "./feed-data.js";
 
-const feedContainer = document.getElementById("feedContainer");
-
-posts.forEach(function (post) {
-  feedContainer.innerHTML += `
-<div class="feed-box">
-    <div class="main-feed-header">
-            <img src="${post.avatar}" class="main-feed-avatar" alt="contributor-avatar" />
-                <div class="main-feed-text">
-                    <h3>${post.name}</h3>
-                    <p>${post.location}</p>
-                </div>
-        </div>
-        <div class="main-feed-img-box">
-            <img src="${post.post}" class="main-feed-img" alt="" id="${post.id}"/>
-        </div>
-        <div class="main-feed-share-icons">
-            <img src="images/like.png" alt="like-icon" class="like-icon" data-like="${post.id}"/>
-            <img src="images/comment.png" alt="comment-icon" class="comment-icon"data-comment="${post.id}"/>
-            <img src="images/share.png" alt="share-icon" class="share-icon" data-share="${post.id}"/>
-        </div>
-        <div class="main-feed-like-count">
-            <p><strong>${post.likes} likes</strong></p>
-            <p><strong>${post.username} </strong>${post.comment}</p>
-        </div>
-</div> 
-    `;
-
-  document.addEventListener("click", function (e) {
-    console.log(e.target.dataset.id);
-  });
+// ensure that the HTML is fully loaded before trying to render the feed.
+document.addEventListener("DOMContentLoaded", function () {
+  render();
 });
+
+// event listener
+
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.like) {
+    handleLikeClick(e.target.dataset.like);
+  }
+});
+
+// handle click function
+function handleLikeClick(postId) {
+  const targetPostObject = posts.filter(function (post) {
+    return post.uuid === postId;
+  })[0];
+
+  if (targetPostObject.isLiked) {
+    targetPostObject.likes--;
+    targetPostObject.isLiked = false;
+  } else {
+    targetPostObject.likes++;
+    targetPostObject.isLiked = true;
+  }
+
+  console.log(targetPostObject.likes);
+  render();
+}
+
+// handle image click
+
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.image) {
+    handleImageClick(e.target.dataset.image);
+  }
+});
+
+function handleImageClick(imageId) {
+  const targetImageObject = posts.filter(function (post) {
+    return post.uuid === imageId;
+  })[0];
+
+  if (targetImageObject.isLiked) {
+    targetImageObject.likes--;
+    targetImageObject.isLiked = false;
+  } else {
+    targetImageObject.likes++;
+    targetImageObject.isLiked = true;
+  }
+
+  console.log(targetImageObject.likes);
+  render();
+}
+
+// rendered HTML
+
+function getFeedHtml() {
+  let feedHtml = "";
+
+  posts.forEach(function (post) {
+    // liked function
+    let likeIconClass = "";
+    if (post.isLiked) {
+      likeIconClass = "liked";
+    }
+
+    feedHtml += `
+            <div class="feed-box">
+                <div class="main-feed-header">
+                        <img src="${post.avatar}" class="main-feed-avatar" alt="contributor-avatar" />
+                            <div class="main-feed-text">
+                                <h3>${post.name}</h3>
+                                <p>${post.location}</p>
+                            </div>
+                    </div>
+                    <div class="main-feed-img-box">
+                        <img src="${post.post.url}" class="main-feed-img ${likeIconClass}" alt="" data-image="${post.uuid}"/>
+                    </div>
+                    <div class="main-feed-share-icons">
+            
+                        <i class="fa-solid fa-heart ${likeIconClass}" data-like="${post.uuid}"></i>
+                     
+                        <i class="fa-regular fa-comment fa-flip-horizontal comment-icon" data-comment="${post.uuid}"></i>
+                   
+                        <i class="fa-regular fa-paper-plane share-icon"   data-share="${post.uuid}"></i>
+            
+                    </div>
+                    <div class="main-feed-like-count">
+                        <p><strong>${post.likes} likes</strong></p>
+                        <p><strong>${post.username} </strong>${post.comment}</p>
+                    </div>
+            </div> 
+                `;
+  });
+  return feedHtml;
+}
+
+function render() {
+  document.getElementById("feedContainer").innerHTML = getFeedHtml();
+}
